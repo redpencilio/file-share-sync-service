525e4e7c9fdb0aeb6d787f620518402b47f16f9f
felix <ruiz.felixruiz@gmail.com>
2021-12-17 17:57:42 +0100

    Update access control flow

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { app, sparqlEscapeUri } from "mu";
+import { app, sparqlEscapeUri, query } from "mu";
 import { querySudo } from '@lblod/mu-auth-sudo';
 import path from "path";
 
@@ -9,41 +9,39 @@ console.log(process.version);
 const ALLOW_SUPER_CONSUMER = process.env.ALLOW_SUPER_CONSUMER == "true" ? true : false;
 
 app.get("/download", async (req, res) => {
-  // 1. get request info
+  // Get request info
   const puri = req.query.uri;
   const name = req.query.name;
   const sessionUri = req.get('mu-session-id');
 
-  // 2. verify the trusted consumer is asking the question
-  const isDiffConsumer = querySudo(`
-    PREFIX muAccount: <http://mu.semte.ch/vocabularies/account/>
-
-    ASK {
-      GRAPH <http://mu.semte.ch/graphs/diff-producer/login> {
-        ${sparqlEscapeUri(sessionUri)} muAccount:account <http://services.lblod.info/diff-consumer/account>.
-       }
-    }`).boolean;
-
-  // 3. construct a response
-  if (!isDiffConsumer) {
-    // A. you're not the consumer
-    res.send(400).send({ error: "Invalid credentials" });
-
-  } else if (!puri) {
-    // B. you didn't supply a file
+  // Validate request
+  if (!puri) {
     res.send(400).send({ error: "No valid URI found as parameter to the URL. Please try a URL like `/download?uri=share://foo`" });
+  }
+  else {
 
-  } else {
-    // C. fetch the lot
-    const filepath = path.normalize(`${SHARE_FOLDER}/${puri.replace("share://", "")}`);
-    res.download(filepath, name, err => {
-      if (err) {
-        console.error(err);
-        res.status(404).end();
-      }
-      else
-        console.log(`Sent file ${filepath} with name ${name}`);
-    });
+    // Validate access:
+    //   - if super-consumer, check session in db
+    //   - else fall back to normal authorization scheme provided by mu-auth
+    const hasAccess = await isValidSuperConsumer(sessionUri) || await hasAccessToFile(puri);
+
+    if (!hasAccess) {
+      // You're not the consumer
+      res.send(400).send({ error: "Invalid credentials" });
+    }
+    else {
+      // All fine: we try to return the file.
+      const filepath = path.normalize(`${SHARE_FOLDER}/${puri.replace("share://", "")}`);
+
+      res.download(filepath, name, err => {
+        if (err) {
+          console.error(err);
+          res.status(404).end();
+        }
+        else
+          console.log(`Sent file ${filepath} with name ${name}`);
+      });
+    }
   }
 });
 
